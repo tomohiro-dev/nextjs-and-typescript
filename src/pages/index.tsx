c829db285c3254e79e5eb847313c91b085f49a3e
tomohiro_sugiyama <tomo.soul.15@gmail.com>
2023-03-23 23:38:09 +0900

    jsで非同期処理を実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,17 @@ import { Fleur_De_Leah, Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
 const Home: NextPage = () => {
+  // APIをたたくときは非同期処理になるのでasync関数で書く
+  const fetchCatImage = async () => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search")
+    const result = res.json();
+    // console.log(result[0]);
+    return result[0];
+  };
+
+  const handleClick = 
+ 
+  
   return (
     <div style={{
       display: "flex",
@@ -12,14 +23,14 @@ const Home: NextPage = () => {
       justifyContent: "center",
       height: "100vh"
 
-    }}>
+    }}> 
       <h1>猫画像アプリ</h1>
       <img 
         src="https://cdn2.thecatapi.com/images/dc5.jpg" alt=""  
         width={500} 
         height="auto"
       />
-      <button>
+      <button onClick={fetchCatImage}>
         今日の猫さん
       </button>
     </div>
