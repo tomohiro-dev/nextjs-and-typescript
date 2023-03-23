f43152bc262c593f870f861ab2990c36ebc9db14
tomohiro_sugiyama <tomo.soul.15@gmail.com>
2023-03-23 23:30:22 +0900

    画像イメージとcssを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import type { NextPage }  from 'next'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Fleur_De_Leah, Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container} >
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100vh"
+
+    }}>
       <h1>猫画像アプリ</h1>
-      <Image src="" alt="" />
-      <button>今日の猫さん</button>
+      <img 
+        src="https://cdn2.thecatapi.com/images/dc5.jpg" alt=""  
+        width={500} 
+        height="auto"
+      />
+      <button>
+        今日の猫さん
+      </button>
     </div>
   )
 
